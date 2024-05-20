27a77063f45d8d7e6a9dd5dccf97737871436969
nilesh7272 <nileshparadva97@gmail.com>
2024-05-20 18:00:54 +0530

    file upload

--- task-1.c
+++ task-1.c
@@ -9,11 +9,17 @@ int main(){
 
 
     if((cha>='a' && cha<='z') || (cha>='A' && cha<='Z')){
+
         printf("Alphabet:- %c\n\n", cha);
+
     }else if(cha>='0' && cha<='9'){
+
         printf("Digit:- %c\n\n", cha);
-    }else {
+
+    }else{
+
         printf("Special character:- %c\n\n", cha);
+
     }
 
     return 0;
--- task-5.c
+++ task-5.c
@@ -29,19 +29,33 @@ int main(){
     int g=f;
 
     if(g<0 || g>=100){
+
         printf("Enter your valid number");
+
     }else if(g<40){
+
         printf("Grade F");
+
     }else if(g>=50 && g<60){
-        printf("Grade E");
+
+        printf("Grade E"); 
+
     }else if(g>=60 && g<70){
+
         printf("Grade D");
+
     }else if(g>=70 && g<80){
+
         printf("Grade C");
+
     }else if(g>=80 && g<90){
+
         printf("Grade B");
+
     }else{
+
         printf("Grade A");
+
     }
    
 
